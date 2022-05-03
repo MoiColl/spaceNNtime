9de6fa985b588996f88d18d4e1a7908c052748cf
qxz396 <qxz396@racimocomp04fl.unicph.domain>
2022-05-03 08:10:10 +0200

    transfer data script

---
+++ backup/scripts/trasnferdata.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/data/
+scp qxz396@racimocomp04fl:/projects/racimolab/people/qxz396/spaceNNtime/data/europe/tree.trees ~/data/.
+scp qxz396@racimocomp04fl:/projects/racimolab/people/qxz396/spaceNNtime/data/europe/metadata.txt ~/data/.
+
+scp qxz396@racimocomp04fl:/projects/racimolab/people/qxz396/spaceNNtime/data/europe/tree.trees ~/data/.
+scp qxz396@racimocomp04fl:/projects/racimolab/people/qxz396/spaceNNtime/data/europe/metadata.txt ~/data/.
+rm -r mkdir -p ~/data
